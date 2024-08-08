edce01019369d50168d46cc4f1117723e8accf5e
Cristhian Vasquez <cristhianvasquez99@gmail.com>
2024-08-08 17:58:28 -0500

    authJWT

--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -4,12 +4,13 @@ import { productRouter } from "./index";
 import { userRouter } from "./userRoutes";
 import { orderRouter } from "./orderRoutes";
 import { cartRouter } from "./cartRoutes";
+import authJWT from "../middlewares/authJWT";
 
 const router: Router = Router();
 
-router.use("/products", productRouter)
+router.use("/products",authJWT, productRouter)
 router.use("/users", userRouter)
-router.use("/orders", orderRouter)
-router.use("/carts", cartRouter)
+router.use("/orders",authJWT, orderRouter)
+router.use("/carts", authJWT, cartRouter)
 
 export default router;
--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -9,3 +9,12 @@ productRouter.post("/", ProductController.createProduct);
 productRouter.put("/:id", ProductController.updateProduct);
 productRouter.delete("/:id", ProductController.deleteProduct);
 
+// enrutar por donde va la peticion 
+
+// middleware capa de autentificacion
+
+// capa controller, recibe las peticiones responde y envia a la capa de service
+
+// capa de service interactua con la capa de datos, hacer las operaciones que tienen que hacer
+
+// capa de modelos es la capa que interactua con la orm utiliza la base de datos
